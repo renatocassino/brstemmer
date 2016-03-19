a7c3f23ea7cfb0a6a777128c15f3d1ce004c030a
Renato Cassino <renatocassino@gmail.com>
2016-03-18 22:51:02 -0300

    Configuration for specs

---
+++ spec/brstemmer_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Brstemmer do
+  pending "write it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'brstemmer'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
